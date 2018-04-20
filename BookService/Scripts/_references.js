f775ce6a3209a399068e9fa3424500b9172eb232
Jesper Rosholm Tørresø <jrt@ase.au.dk>
2018-04-20 16:31:48 +0200

    Opatering af alle komponenter for BookService og VenueService

--- BookService/Scripts/_references.js
+++ BookService/Scripts/_references.js
@@ -1,5 +1,5 @@
-﻿/// <reference path="modernizr-2.8.3.js" />
-/// <reference path="jquery-3.2.1.js" />
+﻿/// <reference path="jquery-3.3.1.js" />
+/// <reference path="modernizr-2.8.3.js" />
 /// <autosync enabled="true" />
 /// <reference path="ai.1.0.0-build00159.min.js" />
 /// <reference path="bootstrap.min.js" />
